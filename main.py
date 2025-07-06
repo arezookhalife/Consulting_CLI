1bb90b54abd9ca33a1027060f1301fa0b16cbe41
arezoo <arezoo.kh1988@gmail.com>
2025-07-06 10:21:37 +0330

    add_consultant() Function/ edit  "Import/create consultants list" part

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
-# Insert cnsultant data by user.
-name= input("نام مشاور:")
-specialty= input("تخصص مntشاور:")
+def add_consultant():
+    """This function helps the user add new consultant data to the consultants.json file."""
+    
+    # Insert consultant data by user.
+    name= input("نام مشاور:")
+    specialty= input("تخصص مشاور:")
 
-# Import cosultants list.
-import json
-with open("consultants.json","r",encoding="utf-8") as f:
-    consultants=json.load(f)
+    # Import/create consultants list.
+    import json
+    try:
+        with open("consultants.json", "r", encoding="utf-8") as f:
+            consultants = json.load(f)
+    except FileNotFoundError:
+        consultants = []
 
-# Create new cosultant with uniq ID
-new_id= consultants[-1]["id"]+ 1 if consultants else 1
-new_consultant = {
-    "id": new_id,
-    "name": name,
-    "specialty": specialty
-}
-consultants.append(new_consultant)
+    # Create a new consultant with a unique ID.
+    new_id= consultants[-1]["id"]+ 1 if consultants else 1
+    new_consultant = {
+        "id": new_id,
+        "name": name,
+        "specialty": specialty
+    }
+    consultants.append(new_consultant)
+    
+    # Saved new data in Json file.
+    with open("consultants.json", "w", encoding= "utf-8") as f:
+        json.dump(consultants,f,ensure_ascii=False,indent=2)
+    print("مشاور با موفقیت افزوده شد!")
 
-# Saved new data in Json file
-with open("consultants.json", "w", encoding= "utf-8") as f:
-    json.dump(consultants,f,ensure_ascii=False,indent=2)
-print("مشاور با موفقیت افزوده شد!")  
+add_consultant()  
   
