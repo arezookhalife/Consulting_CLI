2db92e39130be7763b39b0310f30d7873b3c863d
arezoo <arezoo.kh1988@gmail.com>
2025-07-09 14:07:20 +0330

    Data Input Control (Date, Time, Appointment)

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import json
 from arabic_reshaper import reshape as re 
 from bidi.algorithm import get_display as gd
+from datetime import datetime
+
 
 def show_menu():
     """This function helps the user select a option from menu."""
     
-    # Show menu.
     print(gd(re("\n ===== منوی اصلی ======")))
     print(gd(re("1.افزودن مشاور")))
     print(gd(re("2.افزودن وقت مشاوره")))
@@ -14,12 +15,35 @@ def show_menu():
     print(gd(re("5.خروج")))
     
     
+def validate_datetime(date_str,time_str):
+    """This function Check date & time format."""
+    
+    try:
+        datetime.strptime(date_str, "%Y-%m-%d")
+        datetime.strptime(time_str,"%H:%M")
+        return True
+    except ValueError:
+        return False
+
+
+def is_duplicate_appointment(appointments,consultant_id,date,time):
+    """This function checks for appointment duplication.""" 
+    
+    for a in appointments:
+        if a["consultant_id"] == int(consultant_id) and a["date"] == date and a["time"] == time:
+            return True
+            break   
+        else:
+            continue
+        return False                 
+    
+        
 def add_consultant():
     """This function helps the user add new consultant data to the consultants.json file."""
     
     # Insert consultant data by user.
-    name= input(gd(re("نام مشاور:")))
-    specialty= input(gd(re("تخصص مشاور:")))
+    name= input(gd(re(":نام مشاور")))
+    specialty= input(gd(re(":تخصص مشاور")))
 
     # Import/create consultants list.
     try:
@@ -46,12 +70,18 @@ def add_consultant():
 def add_appointment():
     """This function helps the user add appointment data to the appointments.json file."""
     
-    # Import consultants list.
+    # Import consultants and appointments list.
     try:
         with open("consultants.json", "r", encoding="utf-8") as f:
             consultants = json.load(f)
     except FileNotFoundError:
         consultants = []
+    
+    try:
+        with open("appointments.json", "r", encoding= "utf-8") as g:
+            appointments = json.load(g)
+    except FileNotFoundError:
+        appointments = []        
         
     # Show consultant data for select.
     if consultants==[]:
@@ -63,13 +93,22 @@ def add_appointment():
             consultant_id_list.append(c['id'])
 
         # Insert appointment data by user.
-        consultant_id= int(input(gd(re("ID مشاور را وارد کنید:"))))
+        consultant_id= input(gd(re(":شماره مشاور را وارد کنید")))
         while int(consultant_id) not in consultant_id_list:
             print(gd(re("کد وارد شده صحیح نمی باشد!")))
-            consultant_id= int(input(gd(re("ID مشاور را وارد کنید:"))))
-        date= input(gd(re("تاریخ مشاوره(نمونه 2025-06-30):")))
-        time= input(gd(re("ساعت مشاوره(نمونه 14:00):")))
-        
+            consultant_id= int(input(gd(re(":شماره مشاور را وارد کنید"))))
+                   
+        # Check date & time format    
+        while True:
+            date= input(gd(re(":تاریخ مشاوره(نمونه YYYY-MM-DD)")))
+            time= input(gd(re(":ساعت مشاوره(نمونه hh:mm)")))
+            if not validate_datetime(date,time):
+                print(gd(re("فرمت تاریخ یا ساعت نادرست است. دوباره تلاش کنید!")))
+            elif is_duplicate_appointment(appointments,consultant_id,date,time):
+                print(gd(re("این نوبت قبلا رزور شده است. دوباره تلاش کنید!")))
+            else:
+                break    
+          
         # Create a new appointment.
         new_appointment = {
             "consultant_id": consultant_id,
@@ -78,15 +117,9 @@ def add_appointment():
         }
         
         # Saved new data in Json file.
-        try:
-            with open("appointments.json", "r", encoding= "utf-8") as f:
-                appointments = json.load(f)
-        except FileNotFoundError:
-            appointments = []
-                
         appointments.append(new_appointment)
-        with open("appointments.json", "w", encoding= "utf-8") as f:
-            json.dump(appointments,f,ensure_ascii=False,indent=2)
+        with open("appointments.json", "w", encoding= "utf-8") as g:
+            json.dump(appointments,g,ensure_ascii=False,indent=2)
         print(gd(re("وقت مشاوره با موفقیت ثبت شد!")))
     
 
@@ -138,11 +171,12 @@ def show_appointments():
         for a in sorted_appointments:
             name = consultant_map.get(a['consultant_id'],"مشاور ناشناس")
             print(gd(re(f"{name} | {a['date']} | {a['time']}")))
+
             
 # menu selection loop.
 while True:
     show_menu()
-    choice= input(gd(re("انتخاب شما:")))       
+    choice= input(gd(re(":انتخاب شما")))       
     
     if choice == "1":
         add_consultant()
